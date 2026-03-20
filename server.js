4b8083d88b4654363138039fd969b7eb9aa6d106
rochagreice <greicelr@gmail.com>
2026-03-20 19:47:26 -0300

    Integra SQLite com servidor Express e adiciona a rota GET /incidentes

--- server.js
+++ server.js
@@ -1,13 +1,14 @@
 //Importações
 
-const express = require("express"); //Framework que cria o servidor e as rotas.
-const { criarBanco } = require("./database"); //A chave que vai abrir a conexão com o banco de dados.
+const express = require("express"); //Framework que cria o servidor e as rotas
+const { criarBanco } = require("./database"); //A chave que vai abrir a conexão com o banco de dados
 
-const app = express(); //Inicialização: Ligando o motor do servidor.
+const app = express(); //Inicialização: Ligando o motor do servidor
 
-app.use(express.json()); // Tradutor: Configura o Express para entender dados enviados no formato JSON.
+app.use(express.json()); //Tradutor: Configura o Express para entender dados enviados no formato JSON
 
 //Criando a Rota Principal /Rota Raiz
+
 app.get("/", (req, res) => {
   //res.send: Envia uma resposta simples (texto, html, json)
   res.send(`
@@ -19,16 +20,22 @@ app.get("/", (req, res) => {
         
         `);
 });
+
 //Porta do servidor
+
 const PORT = 3000;
+
 //Ligando o Servidor
 app.listen(PORT, () => {
   console.log(`Servidor rodando em http://localhost:${PORT}`);
 });
 
 //Rota de Listagem - Para buscar todos os problemas registrados
+
 app.get("/incidentes", async (req, res) => {
   const db = await criarBanco(); // Chamamos a função do outro arquivo. O await é o "aguarde", pois o banco precisa de tempo para abrir.
+
   const listaIncidentes = await db.all(`SELECT * FROM incidentes`);
+
   res.json(listaIncidentes); //Entrega esses dados para o cliente em formato JSON
 });
